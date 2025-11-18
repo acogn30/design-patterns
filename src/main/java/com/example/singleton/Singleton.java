ea3aeb01340fe775ed7d75095862bf698e55adf0
acogn30 <acogn30@gmail.com>
2025-11-18 23:00:24 +0800

    Initialize design-patterns project

---
+++ src/main/java/com/example/singleton/Singleton.java
@@ -0,0 +1,8 @@
+package com.example.singleton;
+
+public class Singleton {
+    
+    public static void run() {
+        System.out.println("demo");
+    }
+}
---
+++ src/test/java/com/example/singleton/SingletonTest.java
@@ -0,0 +1,13 @@
+package com.example.singleton;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import org.junit.jupiter.api.Test;
+
+public class SingletonTest {
+    
+    @Test
+    void placeholderTest(){
+        assertTrue(true);
+    }
+}
